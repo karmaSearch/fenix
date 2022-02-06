0c93fb41c1333bb24c6acfd2c29de5db2795152d
Lilla <baldovini.lilla@gmail.com>
2022-02-06 18:59:38 +0100

    replace "learn & act" header image to text

--- app/src/main/java/org/mozilla/fenix/home/learnandact/viewholders/LearnAndActViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/home/learnandact/viewholders/LearnAndActViewHolder.kt
@@ -1,42 +1,30 @@
 package org.mozilla.fenix.home.learnandact.viewholders
 
-import android.net.Uri
-import android.util.Log
 import android.view.View
 import androidx.annotation.Dimension
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.compose.ui.unit.*
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.recyclerview.widget.RecyclerView
 import karma.service.learnandact.LearnAndAct
 import mozilla.components.lib.state.ext.observeAsComposableState
@@ -44,9 +32,9 @@ import mozilla.components.support.images.compose.loader.Fallback
 import mozilla.components.support.images.compose.loader.ImageLoader
 import mozilla.components.support.images.compose.loader.Placeholder
 import mozilla.components.support.images.compose.loader.WithImage
+import mozilla.components.ui.colors.PhotonColors
 import org.mozilla.fenix.R
 import org.mozilla.fenix.components.components
-import org.mozilla.fenix.compose.*
 import org.mozilla.fenix.home.HomeFragmentStore
 import org.mozilla.fenix.home.learnandact.LearnAndActInteractor
 import org.mozilla.fenix.theme.FirefoxTheme
@@ -100,16 +88,15 @@ fun LearnAndAct(
     print(onBlockClicked)
     Column {
         Spacer(Modifier.height(5.dp))
-        androidx.compose.foundation.Image(
-            painter = painterResource(R.drawable.ic_learnandact_header),
-            contentDescription = null,
+        Text("Learn & Act",
+            fontSize = 38.sp,
+            fontWeight = FontWeight.Normal,
+            color = PhotonColors.Green50,
+            fontFamily = FontFamily(Font(R.font.amithen)),
             modifier = Modifier
-                .height(35.dp)
-                .padding(start = 10.dp),
-            contentScale = ContentScale.FillHeight
-        )
+            .padding(start = 10.dp))
 
-        Spacer(Modifier.height(7.dp))
+        Spacer(Modifier.height(5.dp))
 
         LearnAndActBlocs(learnAndActBlocs ?: emptyList(), onBlockClicked)
 
