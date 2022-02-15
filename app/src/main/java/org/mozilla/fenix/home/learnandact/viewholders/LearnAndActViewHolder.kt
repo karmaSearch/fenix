b013293d438ea59c914cf0643ff9ac8f30560b9c
Lilla <baldovini.lilla@gmail.com>
2022-02-15 18:41:02 +0100

    change learn & act UI for tablet

--- app/src/main/java/org/mozilla/fenix/home/learnandact/viewholders/LearnAndActViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/home/learnandact/viewholders/LearnAndActViewHolder.kt
@@ -1,7 +1,6 @@
 package org.mozilla.fenix.home.learnandact.viewholders
 
 import android.view.View
-import androidx.annotation.Dimension
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -16,12 +15,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.booleanResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,10 +58,7 @@ class LearnAndActViewHolder(
                 LearnAndAct(
                     store,
                     interactor::onBlockShown,
-                    interactor::onBlockClicked,
-                    with(composeView.resources) {
-                        getDimensionPixelSize(R.dimen.home_item_horizontal_margin) / displayMetrics.density
-                    }
+                    interactor::onBlockClicked
                 )
             }
         }
@@ -72,8 +70,7 @@ class LearnAndActViewHolder(
 fun LearnAndAct(
     store: HomeFragmentStore,
     onBlockShown: (List<LearnAndAct>) -> Unit,
-    onBlockClicked: (LearnAndAct) -> Unit,
-    @Dimension horizontalPadding: Float = 0f
+    onBlockClicked: (LearnAndAct) -> Unit
 ) {
     val learnAndActBlocs = store
         .observeAsComposableState { state -> state.learnAndAct }.value
@@ -84,8 +81,6 @@ fun LearnAndAct(
             onBlockShown(it)
         }
     }
-    print(horizontalPadding)
-    print(onBlockClicked)
     Column {
         Spacer(Modifier.height(5.dp))
         Text("Learn & Act",
@@ -105,6 +100,58 @@ fun LearnAndAct(
 
 @OptIn(ExperimentalUnitApi::class)
 @Composable
+fun LearnAndActTextsColumn(
+    y: Dp,
+    item: LearnAndAct
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(0.dp),
+        modifier = Modifier
+            .offset(y = y)
+            .padding(start = 10.dp, end = 10.dp, top = 0.dp, bottom = 5.dp)
+    ) {
+        Text(
+            text = item.title,
+            lineHeight = 19.sp,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = FirefoxTheme.colors.textPrimary,
+            fontFamily = FontFamily(Font(R.font.proximanova_semibold)),
+        )
+        if (item.duration.isNotEmpty()) {
+            Text(
+                text = item.duration,
+                lineHeight = 17.sp,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.LightGray,
+                fontFamily = FontFamily(Font(R.font.proximanova_medium)),
+            )
+        }
+        Text(
+            text = item.description,
+            lineHeight = 20.sp,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = FirefoxTheme.colors.textPrimary,
+            maxLines = 4,
+            fontFamily = FontFamily(Font(R.font.proximanova_medium)),
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = item.action,
+            lineHeight = 20.sp,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = FirefoxTheme.colors.brandColor,
+            fontFamily = FontFamily(Font(R.font.proximanova_bold)),
+        )
+    }
+
+}
+
+@OptIn(ExperimentalUnitApi::class)
+@Composable
 fun LearnAndActBlocs(
     learnAndActBlocs: List<LearnAndAct>,
     onStoryClicked: (LearnAndAct) -> Unit
@@ -120,101 +167,120 @@ fun LearnAndActBlocs(
 
             val defaultImageName = if(item.type.lowercase() == "learn" || item.type.lowercase() == "comprendre") R.drawable.ic_learn_placeholder else R.drawable.ic_act_placeholder
 
-            Column(verticalArrangement = Arrangement.spacedBy(0.dp)) {
-                ImageLoader(
-                    url = item.imageUrl,
-                    client = components.core.client
-                ) {
-                    WithImage { painter ->
-                        androidx.compose.foundation.Image(
-                            painter = painter,
-                            modifier = Modifier
-                                .height(193.dp)
-                                .fillMaxWidth(),
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop
-                        )
+            if (booleanResource(id = R.bool.learn_and_act_large_view)) {
+
+                Row(modifier = Modifier.fillMaxWidth()
+                    .heightIn(max = 200.dp),
+                    horizontalArrangement = Arrangement.SpaceAround) {
+
+                    ImageLoader(
+                        url = item.imageUrl,
+                        client = components.core.client
+                    ) {
+                        WithImage { painter ->
+                            androidx.compose.foundation.Image(
+                                painter = painter,
+                                modifier = Modifier.weight(8f, true),
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth
+                            )
+                        }
+
+                        Placeholder {
+                            androidx.compose.foundation.Image(
+                                painter = painterResource(id = defaultImageName),
+                                modifier = Modifier.weight(8f, true),
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth
+                            )
+                        }
+
+                        Fallback {
+                            androidx.compose.foundation.Image(
+                                painter = painterResource(id = defaultImageName),
+                                modifier = Modifier.weight(8f, true),
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth
+                            )
+                        }
                     }
 
-                    Placeholder {
-                        androidx.compose.foundation.Image(
-                            painter = painterResource(id = defaultImageName),
-                            contentDescription = null,
+                    Column(
+                        modifier = Modifier.weight(10f, true)) {
+                        Text(
+                            text = item.type.uppercase(),
+                            lineHeight = 22.sp, fontSize = 16.sp, fontWeight = FontWeight.SemiBold,
                             modifier = Modifier
-                                .height(193.dp)
-                                .fillMaxWidth(),
-                            contentScale = ContentScale.FillWidth
+                                .clip(RoundedCornerShape(0.dp, 12.dp, 12.dp, 0.dp))
+                                .background(FirefoxTheme.colors.brandColor)
+                                .padding(start = 17.dp, end = 17.dp, bottom = 0.dp)
+                                .height(24.dp),
+                            color = Color.Black,
+                            fontFamily = FontFamily(Font(R.font.proximanova_semibold)),
                         )
+
+                        LearnAndActTextsColumn(item = item, y = 0.dp)
                     }
 
-                    Fallback {
-                        androidx.compose.foundation.Image(
-                            painter = painterResource(id = defaultImageName),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .height(193.dp)
-                                .fillMaxWidth(),
-                            contentScale = ContentScale.FillWidth
-                        )
-                    }
+
                 }
-                Text(
-                    text = item.type.uppercase(),
-                    lineHeight = 22.sp, fontSize = 16.sp, fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier
-                        .offset(y = -12.dp)
-                        .clip(RoundedCornerShape(0.dp, 12.dp, 12.dp, 0.dp))
-                        .background(FirefoxTheme.colors.brandColor)
-                        .padding(start = 17.dp, end = 17.dp, bottom = 0.dp)
-                        .height(24.dp) ,
-                    color = Color.Black,
-                    fontFamily = FontFamily(Font(R.font.proximanova_semibold)),
-                )
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(0.dp),
-                    modifier = Modifier
-                        .offset(y = -10.dp)
-                        .padding(start = 10.dp, end = 10.dp, top = 0.dp, bottom = 5.dp)
-                ) {
+            } else {
+                Column(verticalArrangement = Arrangement.spacedBy(0.dp)) {
+                    ImageLoader(
+                        url = item.imageUrl,
+                        client = components.core.client
+                    ) {
+                        WithImage { painter ->
+                            androidx.compose.foundation.Image(
+                                painter = painter,
+                                modifier = Modifier
+                                    .height(193.dp)
+                                    .fillMaxWidth(),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+
+                        Placeholder {
+                            androidx.compose.foundation.Image(
+                                painter = painterResource(id = defaultImageName),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .height(193.dp)
+                                    .fillMaxWidth(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+
+                        Fallback {
+                            androidx.compose.foundation.Image(
+                                painter = painterResource(id = defaultImageName),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .height(193.dp)
+                                    .fillMaxWidth(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+
                     Text(
-                        text = item.title,
-                        lineHeight = 19.sp,
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        color = FirefoxTheme.colors.textPrimary,
+                        text = item.type.uppercase(),
+                        lineHeight = 22.sp, fontSize = 16.sp, fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier
+                            .offset(y = -12.dp)
+                            .clip(RoundedCornerShape(0.dp, 12.dp, 12.dp, 0.dp))
+                            .background(FirefoxTheme.colors.brandColor)
+                            .padding(start = 17.dp, end = 17.dp, bottom = 0.dp)
+                            .height(24.dp),
+                        color = Color.Black,
                         fontFamily = FontFamily(Font(R.font.proximanova_semibold)),
                     )
-                    if (item.duration.isNotEmpty()) {
-                        Text(
-                            text = item.duration,
-                            lineHeight = 17.sp,
-                            fontSize = 12.sp,
-                            fontWeight = FontWeight.Medium,
-                            color = Color.LightGray,
-                            fontFamily = FontFamily(Font(R.font.proximanova_medium)),
-                        )
-                    }
-                    Text(
-                        text = item.description,
-                        lineHeight = 20.sp,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Medium,
-                        color = FirefoxTheme.colors.textPrimary,
-                        maxLines = 4,
-                        fontFamily = FontFamily(Font(R.font.proximanova_medium)),
-                        overflow = TextOverflow.Ellipsis
-                    )
-                    Text(
-                        text = item.action,
-                        lineHeight = 20.sp,
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = FirefoxTheme.colors.brandColor,
-                        fontFamily = FontFamily(Font(R.font.proximanova_bold)),
-                    )
+
+                    LearnAndActTextsColumn(item = item, y = -10.dp)
+
                 }
             }
-
         }
 
         Spacer(modifier = Modifier.height(25.dp))
