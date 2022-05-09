2804cacf77cb2fdbc2e8bc25673143d636ab2f7c
Lilla <baldovini.lilla@gmail.com>
2022-05-09 19:36:35 +0200

    fix onboarding button title

--- app/src/main/java/org/mozilla/fenix/home/onboarding/OnBoarding.kt
+++ app/src/main/java/org/mozilla/fenix/home/onboarding/OnBoarding.kt
@@ -1,6 +1,5 @@
 package org.mozilla.fenix.home.onboarding
 
-import android.graphics.drawable.Drawable
 import org.mozilla.fenix.R
 
 data class OnBoarding(val title: Int,
@@ -8,4 +7,4 @@ val subTitle: Int,
 val backgroundImage: Int,
 val icon: Int,
 val images: List<Int>,
-val nextButtonTitle: Int = R.string.onboarding_skip)
+val nextButtonTitle: Int = R.string.onboarding_next)
