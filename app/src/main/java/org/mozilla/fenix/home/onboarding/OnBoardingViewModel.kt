f42ffc013af0bbf9ed4ef6e2c161ae8a15c21010
Lilla <baldovini.lilla@gmail.com>
2022-05-17 21:39:27 +0200

    fix UI

--- app/src/main/java/org/mozilla/fenix/home/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/org/mozilla/fenix/home/onboarding/OnBoardingViewModel.kt
@@ -15,11 +15,18 @@ class OnBoardingViewModel : ViewModel() {
     var onBoardingPages: List<OnBoarding>
 
     init {
-        onBoardingPages =
-            listOf(OnBoarding(R.string.onboarding_title_1, R.string.onboarding_subtitle_1, R.drawable.onboarding_1, R.drawable.onboarding_illus_1, emptyList()),
+
+        onBoardingPages = if (LocaleManager.getSystemDefault().country == "FR") {
+            listOf(OnBoarding(R.string.onboarding_title_1, R.string.onboarding_subtitle_1, R.drawable.onboarding_1, R.drawable.onboarding_illus_1, listOf(R.drawable.ic_logo_aspas, R.drawable.ic_l214, R.drawable.ic_naat)),
                 OnBoarding(R.string.onboarding_title_2, R.string.onboarding_subtitle_2, R.drawable.onboarding_2, R.drawable.onboarding_illus_2, emptyList()),
                 OnBoarding(R.string.onboarding_title_3, R.string.onboarding_subtitle_3, R.drawable.onboarding_3, R.drawable.onboarding_illus_3, emptyList(), R.string.onboarding_finish))
 
-    }
+        } else {
+            listOf(OnBoarding(R.string.onboarding_title_1, R.string.onboarding_subtitle_1, R.drawable.onboarding_1, R.drawable.onboarding_illus_1, emptyList()),
+                OnBoarding(R.string.onboarding_title_2, R.string.onboarding_subtitle_2, R.drawable.onboarding_2, R.drawable.onboarding_illus_2, emptyList()),
+                OnBoarding(R.string.onboarding_title_3, R.string.onboarding_subtitle_3, R.drawable.onboarding_3, R.drawable.onboarding_illus_3, emptyList(), R.string.onboarding_finish))
+        }
+
+      }
 
 }
