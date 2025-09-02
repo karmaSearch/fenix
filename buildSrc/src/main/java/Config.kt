74f702025e8f08112ba140c3a9bb4ba8babe8b55
Lilla <baldovini.lilla@gmail.com>
2025-09-02 15:09:43 +0200

    migrate to sdk 35

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -13,7 +13,7 @@ object Config {
     // Synchronized build configuration for all modules
     const val compileSdkVersion = 34
     const val minSdkVersion = 21
-    const val targetSdkVersion = 34
+    const val targetSdkVersion = 35
 
     @JvmStatic
     private fun generateDebugVersionName(): String {
